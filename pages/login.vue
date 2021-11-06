ed99c2cfe1e0a9273344a36941260fd3dee152c1
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-11-06 20:29:15 +0000

    aded the card  for login

--- pages/login.vue
+++ pages/login.vue
@@ -2,5 +2,38 @@
   <div>
     <h2>login</h2>
     <p>set up the login input boxes and succefully login</p>
+  
+  <el-card class="box-card">
+  <div slot="header" class="clearfix">
+    <span>Card name</span>
+    <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button>
   </div>
+  <div v-for="o in 4" :key="o" class="text item">
+    {{'List item ' + o }}
+  </div>
+</el-card>
+
+</div>
 </template>
+<style>
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 480px;
+  }
+</style>
