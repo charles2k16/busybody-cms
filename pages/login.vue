dc4a89800a834a1545fbdd29efd8e0c7345bda09
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-11-07 22:50:53 +0000

    creating login form

--- pages/login.vue
+++ pages/login.vue
@@ -3,18 +3,85 @@
     <h2>login</h2>
     <p>set up the login input boxes and succefully login</p>
   
-  <el-card class="box-card">
+  <el-card class="box-card"  style="float: center">
   <div slot="header" class="clearfix">
-    <span>Card name</span>
-    <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button>
+    <span style="float: center; padding: 3px 0">Login </span>
+    <!-- <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button> -->
   </div>
-  <div v-for="o in 4" :key="o" class="text item">
-    {{'List item ' + o }}
-  </div>
+   <el-form
+    ref="dynamicValidateForm"
+    :model="dynamicValidateForm"
+    label-width="40px"
+    class="demo-dynamic"
+  >
+
+  <el-form-item
+      prop="email"
+      label="Email"
+      :rules="[
+        {
+          required: true,
+          message: 'Please input email address',
+          trigger: 'blur',
+        },
+        {
+          type: 'email',
+          message: 'Please input correct email address',
+          trigger: ['blur', 'change'],
+        },
+      ]"
+    >
+     </el-form-item>
+    </el-form>
+ 
 </el-card>
 
 </div>
 </template>
+<script lang="ts">
+export default {
+  data() {
+    return {
+      dynamicValidateForm: {
+        domains: [
+          {
+            key: 1,
+            value: '',
+          },
+        ],
+        email: '',
+      },
+    }
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    },
+    removeDomain(item) {
+      const index = this.dynamicValidateForm.domains.indexOf(item)
+      if (index !== -1) {
+        this.dynamicValidateForm.domains.splice(index, 1)
+      }
+    },
+    addDomain() {
+      this.dynamicValidateForm.domains.push({
+        key: Date.now(),
+        value: '',
+      })
+    },
+  },
+}
+</script>
 <style>
   .text {
     font-size: 14px;
