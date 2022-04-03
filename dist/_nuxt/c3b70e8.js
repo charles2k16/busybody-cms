445267c166f8e2252d1c772ef45c2e1dd2f8938d
Charles Kwabena <charlesprogram@gmail.com>
2022-04-03 22:49:29 +0000

    added dist

---
+++ dist/_nuxt/c3b70e8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{440:function(e,n,t){"use strict";t.r(n);var r={},c=t(18),component=Object(c.a)(r,(function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)}),[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("Dashboard")])])}],!1,null,"03ee4220",null);n.default=component.exports}}]);
